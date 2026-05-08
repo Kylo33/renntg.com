823e317bc4350daf2dc77b349e419a21c8dc205b
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2026-05-08 16:24:46 -0600

    ci: add renntg.com GCP bucket

--- infra/main.tf
+++ infra/main.tf
@@ -15,3 +15,24 @@ provider "google" {
   region = "us-central1"
   zone = "us-central1-c"
 }
+
+resource "google_storage_bucket" "static_site" {
+  name = "renntg.com"
+  location = "US"
+
+  force_destroy = true
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_iam_member" "public_rule" {
+  bucket = google_storage_bucket.static_site.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
+
+resource "google_storage_bucket_object" "indexpage" {
+  name         = "index.html"
+  content      = "<html><body>Hello World!</body></html>"
+  content_type = "text/html"
+  bucket       = google_storage_bucket.static_site.id
+}
