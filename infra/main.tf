f37e948c1a4169cae302f48bb01f1ef885acd9f0
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2026-05-08 17:35:07 -0600

    ci: add IP, urlmap, proxy, and forewarding

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,5 @@
+# Terraform Setup
+
 terraform {
   required_providers {
     google = {
@@ -16,7 +18,8 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-# Static site bucket
+# Static Site Bucket
+
 resource "google_storage_bucket" "static_site" {
   name     = "renntg.com"
   location = "US"
@@ -49,7 +52,38 @@ resource "google_compute_backend_bucket" "static_site" {
   enable_cdn  = true
 }
 
+# Load Balancer Setup
+
 resource "google_compute_global_address" "static" {
   name = "ipv4-address"
 }
 
+resource "google_compute_url_map" "default" {
+  name        = "renntg-urlmap"
+  default_service = google_compute_backend_bucket.static_site.id
+}
+
+resource "google_compute_managed_ssl_certificate" "default" {
+  name = "renntg-cert"
+  managed {
+    domains = ["renntg.com"]
+  }
+}
+
+resource "google_compute_target_https_proxy" "default" {
+  name = "renntg-proxy"
+  url_map = google_compute_url_map.default.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "renntg-forwarding-rule"
+  target     = google_compute_target_https_proxy.default.id
+  ip_address = google_compute_global_address.static.id
+  port_range = 443
+}
+
+output "load_balancer_ip" {
+  value = google_compute_global_address.static.address
+  description = "Load balancer IP address"
+}
